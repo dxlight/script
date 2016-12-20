7e8f501c874fd42e1feeda5aac8829c96ee4739a
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-20 15:36:21 +0100

    Create plot.sh

---
+++ plot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+nounce=20000000000
+till=20250000000
+step=800000
+ram=200000
+for ((z=$nounce;z<=$till;z+$step))
+do
+   usep=$(df -hl | awk '/^\/dev\/xvda1/ { sum+=$5 } END { print sum }')
+   while [ $usep -ge 70 ]
+   do
+      echo RUNNING LOW ON HDD - Waiting for gdrive upload
+      for ((i=60;i>=0;i--))
+      do
+         echo $i - RUNNING LOW ON HDD - Waiting for gdrive upload
+         sleep 1
+      done
+   usep=$(df -hl | awk '/^\/dev\/xvda1/ { sum+=$5 } END { print sum }')
+   done
+/home/ubuntu/mdcct/plotavx2 -x 1 -k 12446025604899037236 -d /home/plots -s $nounce -n $step -m $ram
+mv /home/plots/* /home/plots/complete
+nounce=$(( $nounce + $step))
+done
+

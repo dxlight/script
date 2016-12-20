4416e8854a4681169af44eee25118fff6dc236ea
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-20 15:29:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-apt-get install lxde tightvncserver firefox terminator make gcc git tmux -y && tightvncserver -geometry 1300x700git clone https://github.com/Mirkic7/mdcctcd mdcctmakemkdir /home/plots && mkdir /home/plots/completewget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=downloadmv uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrivechmod +x gdrivesudo install gdrive /usr/local/bin/gdrive
+apt-get install lxde tightvncserver firefox terminator make gcc git tmux -y 
+tightvncserver -geometry 1300x700
+git clone https://github.com/Mirkic7/mdcctcd mdcctmake
+mkdir /home/plots 
+mkdir /home/plots/complete
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
+mv uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive

cde8ea428dbcaf7c78d76a8773729a87de745f56
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-20 15:30:55 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 apt-get install lxde tightvncserver firefox terminator make gcc git tmux -y 
 tightvncserver -geometry 1300x700
-git clone https://github.com/Mirkic7/mdcctcd mdcctmake
+git clone https://github.com/Mirkic7/mdcct
+cd mdcct
+make
 mkdir /home/plots 
 mkdir /home/plots/complete
 wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download

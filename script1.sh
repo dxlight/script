2cc2a1a74e46917150c085b1b86ee527d584c88f
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-20 15:33:07 +0100

    Update and rename script.sh to script1.sh

--- script1.sh
+++ script1.sh
@@ -6,6 +6,3 @@ make
 mkdir /home/plots 
 mkdir /home/plots/complete
 wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
-mv uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive

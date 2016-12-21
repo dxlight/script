cca5e98db9cb3dc4cdaa0b7ea6d4b40960ccc82e
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-21 09:25:35 +0100

    Update gdrive1.sh

--- gdrive1.sh
+++ gdrive1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FILE=""
-DIR="/home/plots/complete1"
+DIR="/home/plots/complete0"
 # init
 # look for empty dir
 while true
 do
 if [ "$(ls -A $DIR)" ]; then
-     datei="$(find /home/plots/complete1/ -type f | head -n1)"
+     datei="$(find /home/plots/complete0/ -type f | head -n1)"
      gdrive upload $datei --timeout 0 --delete --config /home/ubuntu/.gdrive1
 else
     sleep 10

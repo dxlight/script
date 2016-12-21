ea86f49bae3b2778e08e900dd18e233fa416d725
dxlight <dxlight@zonemail.clpccd.edu>
2016-12-21 09:05:14 +0100

    Update gdrive1.sh

--- gdrive1.sh
+++ gdrive1.sh
@@ -7,7 +7,7 @@ while true
 do
 if [ "$(ls -A $DIR)" ]; then
      datei="$(find /home/plots/complete/ -type f | head -n1)"
-     gdrive upload $datei --timeout 0 --delete
+     gdrive upload $datei --timeout 0 --delete --config /home/ubuntu/.gdrive1
 else
     sleep 10
 fi
